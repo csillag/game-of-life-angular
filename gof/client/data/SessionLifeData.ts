53eb9aed8ea63edb74ebc423ff9228cb5ea98aeb
csillag <csillag@harmacolindor.com>
2016-10-11 14:11:32 +0200

    Added SessionLifeData,
    which is a reactive implemtation of the LifeData interface,
    based on Meteor Sessions.

---
+++ gof/client/data/SessionLifeData.ts
@@ -0,0 +1,43 @@
+import { Session } from 'meteor/session';
+
+import { LifeData } from './LifeData';
+
+// A reactive implementation of the data store for the cellular automaton,
+// based on Meteor's Session.
+export class SessionLifeData implements LifeData {
+
+    protected width:number;
+    protected height:number;
+
+    public getWidth():number {
+        return this.width;
+    }
+
+    public getHeight():number {
+        return this.height;
+    }
+
+    protected getKey(x:number, y:number):string {
+        return "life-cell-" + x + "-" + y;
+    }
+
+    public isPopulated(x:number, y:number):boolean {
+        return !!Session.get(this.getKey(x, y));
+    }
+
+    public setPopulated(x:number, y:number, populated:boolean) {
+        Session.set(this.getKey(x, y), populated);
+    }
+
+    constructor(width: number, height: number, reset?: boolean) {
+        this.width = width;
+        this.height = height;
+        if (reset) {
+            for (let x = 0; x < width; x++) {
+                for (let y = 0; y < height; y++) {
+                    this.setPopulated(x, y, false);
+                }
+            }
+        }
+    }
+}
