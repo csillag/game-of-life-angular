5b6549037798233f54d6546159db83eb589279f7
csillag <csillag@harmacolindor.com>
2016-10-11 22:04:04 +0200

    Simplify calculation logic

--- gof/client/data/engine.ts
+++ gof/client/data/engine.ts
@@ -14,19 +14,26 @@ export function evolve(data: LifeData) {
     // We will collects the pending changes here
     const changes:Coord[] = [];
 
+    // Declare variables that will be used for counting
+    let friends:number; // The number of neighbors of a cell
+    let min_x:number;
+    let max_x:number;
+    let min_y:number;
+    let max_y:number;
+    let populated:boolean;
+
     // Go over all the cells
     for (let x = 0; x < width; x++) {
+        // Determine the cols we have to look at for neighbors
+        min_x = (x > 0) ? x-1 : 0;
+        max_x = (x < width) ? x+1 : x;
         for (let y = 0; y < height; y++) {
-            const here = { x, y };
+            // Determine the rows we have to look at for neighbors
+            min_y = (y > 0) ? y-1 : 0;
+            max_y = (y < height) ? y+1 : y;
 
             // Is this cell current populated?
-            let populated = data.isPopulated(x, y);
-
-            // Determine the area where to count the neighbours
-            const min_x = (x > 0) ? x-1 : 0;
-            const max_x = (x < width) ? x+1 : x;
-            const min_y = (y > 0) ? y-1 : 0;
-            const max_y = (y < height) ? y+1 : y;
+            populated = data.isPopulated(x, y);
 
             // Count the neighbors within this area
             let friends = 0;
@@ -41,21 +48,18 @@ export function evolve(data: LifeData) {
             }
 
             // OK, now we have all the data, let's make
-            // some decisions!
-            if (populated) {
-                if ((friends <= 1) || (friends >= 4)) {
-                    // Each cell with one or no neighbors
-                    //   dies, as if by solitude.
-                    // Each cell with four or more neighbors dies,
-                    //   as if by overpopulation.
-                    changes.push(here);
-                }
-                // Each cell with two or three neighbors survives.
-            } else {
-                if (friends == 3) {
-                    // Each cell with three neighbors becomes populated.
-                    changes.push(here);
-                }
+            // some decisions! The rules are:
+            //
+            // Each cell with one or no neighbors dies,
+            //     as if by solitude.
+            // Each cell with four or more neighbors dies,
+            //     as if by overpopulation.
+            // Each cell with three neighbors becomes populated.
+            if (
+                (populated && ((friends <= 1) || (friends >= 4))) ||
+                (!populated && (friends == 3))
+            ) {
+                changes.push({x, y});
             }
         }
     }
