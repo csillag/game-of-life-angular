b2120050a1bae6cd775c33e49eb02fdd24a0d940
csillag <csillag@harmacolindor.com>
2016-10-12 00:41:57 +0200

    Enhance the controls: support setting the size

--- gof/client/ui/App.tsx
+++ gof/client/ui/App.tsx
@@ -21,20 +21,30 @@ export class App extends React.Component<{}, AppState> {
         };
         this.actions = {
             reset: () => {
-                const life = new SessionLifeData(10, 10, true);
+                let life = this.state.life;
+                if (life) { // Do we already have a data store?
+                    life.clear(); // If yes, just clear it
+                } else { // If no,
+                    // Create a new one
+                    life = new SessionLifeData(10, 10, true);
+                    // And save it to the component state
+                    this.setState({ life });
+                }
+                // Set the initial pattern
                 life.setPopulated(1, 2, true);
                 life.setPopulated(2, 2, true);
                 life.setPopulated(3, 2, true);
                 life.setPopulated(3, 1, true);
                 life.setPopulated(2, 0, true);
-
-                // Put the created data into the component's state
-                this.setState({
-                    life,
-                });
             },
             evolve: () => {
                 evolve(this.state.life);
+            },
+            setWidth: (value:number) => {
+                this.state.life.setWidth(value);
+            },
+            setHeight: (value:number) => {
+                this.state.life.setHeight(value);
             },
         };
     }
@@ -53,8 +63,12 @@ export class App extends React.Component<{}, AppState> {
                 See the source <a target="_bland" href="https://github.com/csillag/game-of-life">on Github</a>.</p>
                 <p>This is a cellular automaton. You can click at the cells in the grid to create/destroy life, or click the "Evolve!" button to apply the automatic rules.</p>
 
+                <Controls
+                    width={this.state.life.getWidth()}
+                    height={this.state.life.getHeight()}
+                    actions={this.actions}
+                />
                 <CellGridMeteorContainer data={this.state.life} />
-                <Controls actions={this.actions} />
             </div>
         );
     }
--- gof/client/ui/Controls.tsx
+++ gof/client/ui/Controls.tsx
@@ -1,31 +1,77 @@
 import * as React from 'react';
+import SyntheticEvent = __React.SyntheticEvent;
+const NumericInput = require('react-numeric-input');
 
 interface ControlActions {
     evolve():void;
     reset():void;
+    setWidth(value:number):void;
+    setHeight(value:number):void;
 }
 
 interface ControlProps {
+    width: number;
+    height: number;
     actions: ControlActions;
 }
 
 export class Controls extends React.Component<ControlProps,{}> {
 
     static propTypes: React.ValidationMap<ControlProps> = {
+        width: React.PropTypes.number.isRequired,
+        height: React.PropTypes.number.isRequired,
         actions: React.PropTypes.object.isRequired,
     };
 
+    protected renderEvolve() {
+        return (<button onClick={this.props.actions.evolve}>
+            Evolve!
+        </button>)
+    }
+
+    protected renderReset() {
+        return (<button onClick={this.props.actions.reset}>
+            Reset
+        </button>)
+    }
+
+    protected onWidthChange(value:number) {
+        this.props.actions.setWidth(value);
+    }
+
+    protected onHeightChange(value:number) {
+        this.props.actions.setHeight(value);
+    }
+
+    protected renderWidth() {
+        return (<NumericInput
+            className="grid-size"
+            min={10}
+            max={20}
+            value={this.props.width}
+            onChange={this.onWidthChange.bind(this)}
+        />)
+    }
+
+    protected renderHeight() {
+        return (<NumericInput
+            className="grid-size"
+            min={10}
+            max={20}
+            value={this.props.width}
+            onChange={this.onHeightChange.bind(this)}
+        />)
+    }
+
     render() {
-        return (
-                <div id="controls">
-                    <button onClick={this.props.actions.evolve}>
-                        Evolve!
-                    </button>
-                    <button onClick={this.props.actions.reset}>
-                        Reset
-                    </button>
-                </div>
-        );
+        return (<div id="controls">
+            { this.renderEvolve() }
+            { this.renderReset() }
+            <span>Width:</span>
+            { this.renderWidth() }
+            <span>Height:</span>
+            { this.renderHeight() }
+        </div>);
     }
 
 }
