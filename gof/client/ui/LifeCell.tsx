bb7feedc8dd8cc10536841391da69ac9c59a645f
csillag <csillag@harmacolindor.com>
2016-10-11 13:45:36 +0200

    Create a meteor-reactive wrapper around LifeCell
    ... so that when using a reactive datasource for LifeData,
    the state will be updated automatically.
    
    Also, make the click handler set the value of the cell.

--- gof/client/ui/LifeCell.tsx
+++ gof/client/ui/LifeCell.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
+import { createContainer } from 'meteor/react-meteor-data';
 
 import { LifeData } from '../data/LifeData';
 
@@ -9,30 +10,30 @@ interface LifeCellProps {
     y: number;
 }
 
-// This react component displays one cell in the grid.
-export class LifeCell extends React.Component<LifeCellProps,{}> {
+interface _LifeCellProps extends LifeCellProps {
+    populated: boolean;
+}
 
-    static propTypes: React.ValidationMap<LifeCellProps> = {
+// This react component displays one cell in the grid.
+class _LifeCell extends React.Component<_LifeCellProps,{}> {
+
+    static propTypes: React.ValidationMap<_LifeCellProps> = {
         data: React.PropTypes.object.isRequired,
         x: React.PropTypes.number.isRequired,
         y: React.PropTypes.number.isRequired,
+        populated: React.PropTypes.bool.isRequired,
     };
 
     private onClick() {
-        console.log("Clicked on cell (",
-                    this.props.x, ";",
-                    this.props.y, ")"
-        );
+        this.props.data.setPopulated(this.props.x, this.props.y,
+                                     !this.props.populated);
     }
 
     public render() {
 
-        const populated = this.props.data.isPopulated(
-            this.props.x, this.props.y
-        );
         const classes = classNames({
             "cell": true,
-            "alive": populated
+            "alive": this.props.populated
         });
         return <td
             className={classes}
@@ -40,3 +41,12 @@ export class LifeCell extends React.Component<LifeCellProps,{}> {
         />;
     }
 }
+
+export const LifeCell = createContainer<LifeCellProps>((props:LifeCellProps) => {
+    return {
+        data: props.data,
+        x: props.x,
+        y: props.y,
+        populated: props.data.isPopulated(props.x, props.y),
+    };
+}, _LifeCell);
