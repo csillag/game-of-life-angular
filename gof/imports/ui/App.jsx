4132878c349288d7ebfc6df2be77cce000317110
csillag <csillag@harmacolindor.com>
2016-10-11 10:36:34 +0200

    Added React.js (instead of Blaze)

---
+++ gof/imports/ui/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// App component - represents the whole app
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Game of Life</h1>
+        </header>
+
+        <p>Content comes here.</p>
+      </div>
+    );
+  }
+}
